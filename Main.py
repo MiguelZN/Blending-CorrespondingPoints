0a6c2371bf17f211729f0834fc893a5507a6bed4
MiguelZN <miguelzavalan@gmail.com>
2020-03-30 16:25:17 -0400

    added input_images directory

---
+++ Main.py
@@ -0,0 +1,44 @@
+import numpy
+
+print('fdsfds')
+
+
+#Part 1:------------------------------
+'''
+I is image of varying size, 
+H is kernel of varying size. 
+Output: convolution result that is displayed. 
+'''
+def Convolve(I,H):
+    return
+
+'''
+takes image I as input and outputs copy of image re-sampled
+by half the width and height of the input. Remember to 
+Gaussian filter the image before reducing it; use separable
+1D Gaussian kernels. 
+'''
+def Reduce(I):
+    return
+
+'''
+takes image I as input and outputs copy of image expanded,
+twice the width and height of the input. '''
+def Expand(I):
+    return
+
+
+'''
+takes in an image I,
+takes in N (int) is the no. of levels. 
+'''
+def GaussianPyrmaid(I,N):
+    return
+
+'''
+function which collapses the Laplacian pyramid LI of nlevels 
+to generate the original image. Report the error in reconstruction
+using image difference. 
+'''
+def Reconstruct(Ll,N):
+    return
